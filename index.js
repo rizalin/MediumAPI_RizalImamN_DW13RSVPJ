746cc0d818dd78c0693c8e1fbbae417ac21c356f
rizalin <55905761+rizalin@users.noreply.github.com>
2019-12-17 23:18:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,17 @@ const app = express()
 const port = 5000
 
 app.use(bodyParser.json())
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Header","*");    
+    res.header("Access-Control-Allow-Method","*");
+    next();
+})
+
+
 const CategoriesController = require('./controllers/categories')
+const ArticlesController = require('./controllers/articles')
 
 app.group("/api/v1", (router) => {
     router.get("/categories", CategoriesController.index)
@@ -14,6 +24,12 @@ app.group("/api/v1", (router) => {
     router.post('/category', CategoriesController.store)
     router.patch('/category/:id', CategoriesController.update)
     router.delete('/category/:id', CategoriesController.delete)
+
+    router.get("/articles", ArticlesController.index)
+    router.get('/article/:id', ArticlesController.show)
+    router.post('/article', ArticlesController.store)
+    router.patch('/article/:id', ArticlesController.update)
+    router.delete('/article/:id', ArticlesController.delete)
 
 })
 
