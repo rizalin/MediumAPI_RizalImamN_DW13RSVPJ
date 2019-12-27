d4e585283db6a28f62a7e77f46c78aaf1ec60a07
rizalin <55905761+rizalin@users.noreply.github.com>
2019-12-28 00:04:44 +0700

    Update 20191217162531-seed-article.js

--- seeders/20191217162531-seed-article.js
+++ seeders/20191217162531-seed-article.js
@@ -8,7 +8,7 @@ module.exports = {
         {
           title: "Why Fake News is Candy to Our Brain",
           content:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur fringilla finibus aliquet. Curabitur eget dignissim justo, at dapibus leo. Vestibulum hendrerit pulvinar vestibulum. Praesent iaculis faucibus feugiat. Donec et mi at nunc sollicitudin accumsan. Nam quis semper diam, non fermentum tellus. Praesent mollis ligula sit amet mauris interdum, quis pharetra ipsum congue. Ut a tortor eleifend, porttitor tellus eu, tempus ex. Morbi scelerisque, tellus ac porttitor ornare, massa tellus iaculis odio, id finibus turpis metus sed augue.",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur fringilla finibus aliquet.",
           brief:
             "Memory bias causes people to hold on to the (true and untrue) stories",
           image: "https://imgur.com/download/vRUSjFt",
@@ -20,7 +20,7 @@ module.exports = {
         {
           title: "How Acupunture Saved Me From Unbearable Knee Pain",
           content:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur fringilla finibus aliquet. Curabitur eget dignissim justo, at dapibus leo. Vestibulum hendrerit pulvinar vestibulum. Praesent iaculis faucibus feugiat. Donec et mi at nunc sollicitudin accumsan. Nam quis semper diam, non fermentum tellus. Praesent mollis ligula sit amet mauris interdum, quis pharetra ipsum congue. Ut a tortor eleifend, porttitor tellus eu, tempus ex. Morbi scelerisque, tellus ac porttitor ornare, massa tellus iaculis odio, id finibus turpis metus sed augue.",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur fringilla finibus aliquet.",
           brief: "Rediscovering mobility in spite of osteoarthritis",
           image: "https://imgur.com/download/vRUSjFt",
           category_id: 3,
@@ -33,7 +33,7 @@ module.exports = {
           brief:
             "Experts can even predict who's likely to suffer or recover, based on brain structure and personality",
           content:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur fringilla finibus aliquet. Curabitur eget dignissim justo, at dapibus leo. Vestibulum hendrerit pulvinar vestibulum. Praesent iaculis faucibus feugiat. Donec et mi at nunc sollicitudin accumsan. Nam quis semper diam, non fermentum tellus. Praesent mollis ligula sit amet mauris interdum, quis pharetra ipsum congue. Ut a tortor eleifend, porttitor tellus eu, tempus ex. Morbi scelerisque, tellus ac porttitor ornare, massa tellus iaculis odio, id finibus turpis metus sed augue.",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur fringilla finibus aliquet.",
           image: "https://imgur.com/download/vRUSjFt",
           category_id: 3,
           author_id: 1,
