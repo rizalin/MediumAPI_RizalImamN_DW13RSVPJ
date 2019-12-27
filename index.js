b918d42d6df685afa3537b63b9d79f4b68eeb5ae
rizalin <rizal.imamnugraha@gmail.com>
2019-12-27 16:28:44 +0700

    heroku config

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const express = require("express");
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
